b7b78a8df29d20b6dbeaef5fa53de52a3977e44a
S235119 <S235119@dtu.dk>
2024-06-13 09:50:26 +0200

    Små ting fixet, stadig problemer med metoderne i repository

--- src/main/java/com/dtu/Roborally/controller/GameInfoController.java
+++ src/main/java/com/dtu/Roborally/controller/GameInfoController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 
+
 @RestController
 @RequestMapping("/gameInfos")
 public class GameInfoController {
